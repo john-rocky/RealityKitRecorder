840f019f316673c7806f08dec8b6d911f73cc102
john-rocky <john-rocky@hubot.com>
2021-12-17 07:56:48 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RealityKitRecorder
+
+A description of this package.
---
+++ Sources/RealityKitRecorder/RealityKitRecorder.swift
@@ -0,0 +1,6 @@
+public struct RealityKitRecorder {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
